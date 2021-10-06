c5b993f933f24390a4baf51cbe10b11a83de0f3e
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-10-06 22:06:25 +0530

    Covid Tracker App

--- app/src/main/java/com/example/covid_tracker_app/ModelViewHolder.java
+++ app/src/main/java/com/example/covid_tracker_app/ModelViewHolder.java
@@ -28,26 +28,26 @@ public class ModelViewHolder extends RecyclerView.ViewHolder {
     }
 
     private void initViews(View itemView) {
-        mTvDate =itemView.findViewById(R.id.tvDate);
-        mTvPositive =itemView.findViewById(R.id.tvPositive);
-        mTvNegative =itemView.findViewById(R.id.tvNegative);
-        mTvHospitalizedCurrently =itemView.findViewById(R.id.tvHospitalizedCurrently);
-        mTvOnVentilatorCurrently =itemView.findViewById(R.id.tvOnVentilatorCurrently);
-        mTvDeath =itemView.findViewById(R.id.tvDeath);
-        mTvDateChecked =itemView.findViewById(R.id.tvDateChecked);
+        mTvDate = itemView.findViewById(R.id.tvDate);
+        mTvPositive = itemView.findViewById(R.id.tvPositive);
+        mTvNegative = itemView.findViewById(R.id.tvNegative);
+        mTvHospitalizedCurrently = itemView.findViewById(R.id.tvHospitalizedCurrently);
+        mTvOnVentilatorCurrently = itemView.findViewById(R.id.tvOnVentilatorCurrently);
+        mTvDeath = itemView.findViewById(R.id.tvDeath);
+        mTvDateChecked = itemView.findViewById(R.id.tvDateChecked);
         mCvCardView = itemView.findViewById(R.id.cvCardView);
     }
 
     public void setData(ResponseModel responseModel) {
-        String Date = responseModel.getDate()+"";
-        String DateChecked = responseModel.getDateChecked()+"";
-        mTvDate.setText("Date: "+Date.substring(0,4)+"-"+Date.substring(4,6)+"-"+Date.substring(6));
-        mTvPositive.setText("Positive: "+responseModel.getPositive());
-        mTvNegative.setText("Negative: "+responseModel.getNegative());
-        mTvHospitalizedCurrently.setText("Hospitalized Currently: "+responseModel.getHospitalizedCurrently());
-        mTvOnVentilatorCurrently.setText("On Ventilator Currently: "+responseModel.getOnVentilatorCurrently());
-        mTvDeath.setText("Death: "+responseModel.getDeath());
-        mTvDateChecked.setText("Date Checked: "+DateChecked.substring(0,10));
+        String Date = responseModel.getDate() + "";
+        String DateChecked = responseModel.getDateChecked() + "";
+        mTvDate.setText("Date: " + Date.substring(0, 4) + "-" + Date.substring(4, 6) + "-" + Date.substring(6));
+        mTvPositive.setText("Positive: " + responseModel.getPositive());
+        mTvNegative.setText("Negative: " + responseModel.getNegative());
+        mTvHospitalizedCurrently.setText("Hospitalized Currently: " + responseModel.getHospitalizedCurrently());
+        mTvOnVentilatorCurrently.setText("On Ventilator Currently: " + responseModel.getOnVentilatorCurrently());
+        mTvDeath.setText("Death: " + responseModel.getDeath());
+        mTvDateChecked.setText("Date Checked: " + DateChecked.substring(0, 10));
 
 //        mCvCardView.setOnClickListener(new View.OnLongClickListener() {
 //            @Override
